1dece0c51c786683e30e743f7a868b705fd396b4
Nkisi <nkisi@pop-os.localdomain>
2022-01-30 15:41:00 -0500

    Add test for get_absolute_url() method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -8,3 +8,9 @@ def test__str__():
     cheese = CheeseFactory(name="Stracchino")
     assert cheese.__str__() == "Stracchino"
     assert str(cheese) == "Stracchino"
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
+    
